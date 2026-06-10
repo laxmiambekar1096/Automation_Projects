35263c8531167d7bad9ad343831cf5efe4470795
laxmiambekar1096 <laxmiambekar1096@gmail.com>
2026-06-10 19:54:08 +0530

    Uploaded my first Automation Project

--- SauceDemo_Automation_Test/src/test/java/helper/BaseTest.java
+++ SauceDemo_Automation_Test/src/test/java/helper/BaseTest.java
@@ -1,20 +1,20 @@
 package helper;
 
-import org.testng.annotations.Test;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 
 import utility_Files.ExtentManage_Utility;
 
 import org.testng.annotations.BeforeMethod;
-
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Properties;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
@@ -26,26 +26,43 @@ public class BaseTest {
 	public ExtentTest test;
 	public static Logger log=LogManager.getLogger(BaseTest.class);
 	
-  @BeforeSuite
-  public void setupReport()
-  {
-	  extent=ExtentManage_Utility.createInstance();  
-	  }
+
   
-  @BeforeMethod
-  public void initialize_n_OpenBrowser(Method method)
-  {
-	  
+  @BeforeSuite
+  public void setupReport() {
+	  extent=ExtentManage_Utility.createInstance();
+  }
+
+   @BeforeMethod
+  public void initialize_n_OpenBrowser(Method method) throws IOException {
+	   test=extent.createTest(method.getName());
+	   prop=new Properties();
+	   FileInputStream fis=new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\config.properties");
+	   prop.load(fis);
+	   
+	   //Simple WebDriver Setup
+	   String browser=prop.getProperty("browser");
+	   if(browser.equalsIgnoreCase("chrome")){
+	   wd=new ChromeDriver();
+	   }
+	   else if(browser.equalsIgnoreCase("firefox")) {
+		   wd=new FirefoxDriver();
+	   }
+	   wd.manage().window().maximize();
+	   wd.get(prop.getProperty("url"));
+	   log.info("Browser launched and navigated to URL");
   }
 
   @AfterMethod
-  public void afterMethod() {
+  public void closeBrowser() {
+	  wd.close();
+	  log.info("Browser Closed");
   }
 
-  
 
   @AfterSuite
-  public void afterSuite() {
+  public void flushReport() {
+	  extent.flush();//This generates the final HTML file
   }
 
 }
